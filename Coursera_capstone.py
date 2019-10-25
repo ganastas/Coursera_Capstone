30c56e8c940f3e09f9341bb2cbf7117f3b507597
ganastas <57005922+ganastas@users.noreply.github.com>
2019-10-25 16:15:39 +0200

    Added

---
+++ Coursera_capstone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for Capstone exercise
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
